849ebb31ecd58bfb970b63c4684ce94c43ef19c8
Gustavo Schneiter <gustavomalleths@gmail.com>
2026-09-26 02:38:05 -0300

    feat: export filesystem search profile

--- src/index.ts
+++ src/index.ts
@@ -81,6 +81,10 @@ export {
   gitProfiles,
 } from "./profiles/git.js";
 export {
+  FileLineSearchProfile,
+  filesystemSearchProfiles,
+} from "./profiles/search.js";
+export {
   MAX_DIAGNOSTICS,
   PROTOCOL_V1,
   ProtocolError,
